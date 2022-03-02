02e09b9d5841fd66aafa70925ef39b481b48f548
JuliaGMartins <julia.geremias.martins@gmail.com>
2022-03-02 17:11:31 -0300

    Contacts icons scale

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -9,20 +9,20 @@ function Contact() {
   return (
     <div>
         <h1 className="sub">
-            Contact
+            Contato
         </h1>
         <div className="ContactImages">
             <a href="https://github.com/JuliaGMartins" target='_blank'>
-                <img src={github} alt="github"></img>
+                <img className="Icon" src={github} alt="github"></img>
             </a>
             <a href="https://www.linkedin.com/in/julia-gmartins/" target='_blank'>
-                <img src={linkedin} alt="linkedin"></img>
+                <img className="Icon" src={linkedin} alt="linkedin"></img>
             </a>
             <a href="mailto:julia.geremias.martins@gmail.com" target='_blank'>
-                <img src={email} alt="gmail"></img>
+                <img className="Icon" src={email} alt="gmail"></img>
             </a>
             <a href="https://api.whatsapp.com/send?phone=5551982321389" target='_blank'>
-                <img src={whatsapp} alt="whatsapp"></img>
+                <img className="Icon" src={whatsapp} alt="whatsapp"></img>
             </a>
         </div>
     </div>
